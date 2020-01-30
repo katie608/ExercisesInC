349e1f968de7e8c655fc4faa85bc06ea855a70ee
katie608 <katief650@gmail.com>
2020-01-29 22:28:15 -0500

    Add third function

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -7,30 +7,6 @@
  #include <stdlib.h>
 
 
- // int getValue (char card) {
- //   int val = 0;
- //   switch(card) {
- //     case 'K':
- //     case 'Q':
- //     case 'J':
- //       val = 10;
- //       break;
- //     case 'A':
- //       val = 11;
- //       break;
- //     case 'X':
- //       continue;
- //     default:
- //       val = atoi(card);
- //       if ((val < 1) || (val > 10)) {
- //         puts("I don't understand that value!");
- //         continue;
- //       }
- //   }
- //   return val;
- // }
-
-
  int keepCount (int card_value) {
    /*
     * takes in the value of the card and returns the change to be made
@@ -45,48 +21,43 @@
    }
  }
 
+ int getValue () {
+   
+   char card_name[3];
+   int count = 0;
 
+ do {
+   puts("Enter the card_name: ");
+   scanf("%2s", card_name);
 
+   int val = 0;
+   switch(card_name[0]) {
+     case 'K':
+     case 'Q':
+     case 'J':
+       val = 10;
+       break;
+     case 'A':
+       val = 11;
+       break;
+     case 'X':
+       continue;
+     default:
+       val = atoi(card_name);
+       if ((val < 1) || (val > 10)) {
+         puts("I don't understand that value!");
+         continue;
+       }
+   }
+
+   count += keepCount(val);
+   printf("Current count: %i\n", count);
+
+ } while(card_name[0] != 'X');
+   return 0;
+ }
 
 
 int main() {
-  char card_name[3];
-  int count = 0;
-
-do {
-
-  puts("Enter the card_name: ");
-  scanf("%2s", card_name);
-
-
-  int val = 0;
-  switch(card_name[0]) {
-    case 'K':
-    case 'Q':
-    case 'J':
-      val = 10;
-      break;
-    case 'A':
-      val = 11;
-      break;
-    case 'X':
-      continue;
-    default:
-      val = atoi(card_name);
-      if ((val < 1) || (val > 10)) {
-        puts("I don't understand that value!");
-        continue;
-      }
-  }
-
-  // int val = getValue(card_name[0]);
-
-  count += keepCount(val);
-
-  printf("Current count: %i\n", count);
-
-} while(card_name[0] != 'X');
-  return 0;
-
-
+  getValue();
  }
