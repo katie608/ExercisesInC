0c2c216b63ec38463f96a58ee6f358fd39a6e8db
katie608 <katief650@gmail.com>
2020-01-29 22:23:00 -0500

    Add cards.c with two functions

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -7,6 +7,30 @@
  #include <stdlib.h>
 
 
+ // int getValue (char card) {
+ //   int val = 0;
+ //   switch(card) {
+ //     case 'K':
+ //     case 'Q':
+ //     case 'J':
+ //       val = 10;
+ //       break;
+ //     case 'A':
+ //       val = 11;
+ //       break;
+ //     case 'X':
+ //       continue;
+ //     default:
+ //       val = atoi(card);
+ //       if ((val < 1) || (val > 10)) {
+ //         puts("I don't understand that value!");
+ //         continue;
+ //       }
+ //   }
+ //   return val;
+ // }
+
+
  int keepCount (int card_value) {
    /*
     * takes in the value of the card and returns the change to be made
@@ -22,17 +46,18 @@
  }
 
 
+
+
+
 int main() {
   char card_name[3];
   int count = 0;
 
-  int done = 0;
-
-while ( card_name[0] != 'X' ) {
-  // I could not get the "do" loop running, even while copyint the exact code from the book
+do {
 
   puts("Enter the card_name: ");
   scanf("%2s", card_name);
+
 
   int val = 0;
   switch(card_name[0]) {
@@ -54,12 +79,14 @@ while ( card_name[0] != 'X' ) {
       }
   }
 
+  // int val = getValue(card_name[0]);
+
   count += keepCount(val);
 
   printf("Current count: %i\n", count);
 
-
-}
+} while(card_name[0] != 'X');
   return 0;
+
 
  }
