e6af877866e08348903919318e154a6a8f26e073
katie608 <katief650@gmail.com>
2020-02-06 18:27:59 -0500

    Add stack allocated data part

--- exercises/ex02/aspace.c
+++ exercises/ex02/aspace.c
@@ -15,12 +15,31 @@ int main ()
     int var2 = 5;
     void *p = malloc(128);
     char *s = "Hello, World";
+    void *p2 = malloc(256);
+    int local = 12;
+    void *p3 = malloc(2);
+    void *p4 = malloc(2);
 
     printf ("Address of main is %p\n", main);
     printf ("Address of var1 is %p\n", &var1);
     printf ("Address of var2 is %p\n", &var2);
     printf ("p points to %p\n", p);
     printf ("s points to %p\n", s);
+    printf ("p2 points to %p\n", p2);
+    printf ("local points to %p\n", local);
+    printf ("p3 points to %p\n", p3);
+    printf ("p4 points to %p\n", p4);
 
     return 0;
 }
+
+
+/*
+The Address Space
+  3. See PDF in this directory for sketch
+  4. Yes, another call to malloc makes the stack grow upwards
+  5. Local has a smaller address
+  6. I used a size of 2, and google says there is 0x20
+    space between them
+
+*/
--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -5,28 +5,42 @@ License: GNU GPLv3
 
 */
 
+/*
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #define SIZE 5
 
 int *foo() {
+  /* makes an array with size of SIZE
+  prints array
+  iterates through array, sets all elements to 42
+  returns array
+  */
     int i;
     int array[SIZE];
 
-    printf("%p\n", array);
+    // see what happens when you take out the frame added by the print statments
+    // printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = 42;
     }
-    return array;
+    return array; // can't return array here
 }
 
 void bar() {
+  /* makes an array of size SIZE
+  prints array
+  iterates through array, set each element of array to its index
+  */
     int i;
     int array[SIZE];
 
-    printf("%p\n", array);
+    // see what happens when you take out the frame added by the print statments
+    // printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = i;
@@ -36,12 +50,40 @@ void bar() {
 int main()
 {
     int i;
+    // initialize array with foo function
     int *array = foo();
+    // calls bar but does not assign it to anything
     bar();
 
+    // iterates through array, prints each element
     for (i=0; i<SIZE; i++) {
         printf("%d\n", array[i]);
     }
 
     return 0;
 }
+
+
+/*
+Stack Allocated Data
+  1. This program first calles foo(), which:
+        makes an array with size of SIZE
+        prints array
+        iterates through array, sets all elements to 42
+        returns array
+      Then it calls bar() which:
+        makes an array of size SIZE
+        prints array
+        iterates through array, set each element of array to its index
+      Then it iterates through the array and prints each value
+
+  2. The warning is:
+        function returns address of local variable [-Wreturn-local-addr]
+     It means that a local variable is used outside of it's "locality" (function),
+     which means it can't be accessed. When the function stops running, its
+     local memory is cleared so it is not helpful to return a pointer to that
+     variable since the pointer will then point to nothing.
+  3. The same error occurs because foo is still returning a pointer to the
+     locally define array
+
+*/
