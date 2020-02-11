3130d059c40a19ea222b302558b89b913220e644
katie608 <katief650@gmail.com>
2020-02-10 20:06:43 -0500

    Fill in find_track_regex function

--- exercises/ex02.5/find_track.c
+++ exercises/ex02.5/find_track.c
@@ -7,6 +7,7 @@ Modified version of an example from Chapter 2.5 of Head First C.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 #define NUM_TRACKS 5
 
@@ -22,8 +23,7 @@ char tracks[][80] = {
 // Finds all tracks that contain the given string.
 //
 // Prints track number and title.
-void find_track(char search_for[])
-{
+void find_track(char search_for[]) {
     int i;
     for (i=0; i<NUM_TRACKS; i++) {
         if (strstr(tracks[i], search_for)) {
@@ -35,22 +35,31 @@ void find_track(char search_for[])
 // Finds all tracks that match the given pattern.
 //
 // Prints track number and title.
-void find_track_regex(char pattern[])
-{
-    // TODO: fill this in
+void find_track_regex(char pattern[]) {
+    regex_t re;
+    int reg = regcomp(&re, pattern, REG_EXTENDED|REG_NOSUB|REG_ICASE);
+    int result;
+    for(int i=0; i <= NUM_TRACKS; i++){
+      result = regexec(&re, tracks[i], (size_t) 0, NULL, 0);
+      if (result == 0) {
+        printf("Track %i: '%s'\n", i, tracks[i]);
+        // exit(0);
+      }
+    }
+
+    exit(1);
+    regfree(&re);
 }
 
 // Truncates the string at the first newline, if there is one.
-void rstrip(char s[])
-{
+void rstrip(char s[]) {
     char *ptr = strchr(s, '\n');
     if (ptr) {
         *ptr = '\0';
     }
 }
 
-int main (int argc, char *argv[])
-{
+int main (int argc, char *argv[]) {
     char search_for[80];
 
     /* take input from the user and search */
@@ -58,8 +67,8 @@ int main (int argc, char *argv[])
     fgets(search_for, 80, stdin);
     rstrip(search_for);
 
-    find_track(search_for);
-    //find_track_regex(search_for);
+    // find_track(search_for);
+    find_track_regex(search_for);
 
     return 0;
 }
