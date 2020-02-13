11e3f1f6fa1f9f74649ef684cc071eb0e49cf8b7
katie608 <katief650@gmail.com>
2020-02-13 18:09:55 -0500

    Get itoc working

--- exercises/ex02.5/bigint.c
+++ exercises/ex02.5/bigint.c
@@ -2,20 +2,19 @@
 
 This program shows a way to represent a BigInt type (arbitrary length integers)
 using C strings, with numbers represented as a string of decimal digits in reverse order.
-
 Follow these steps to get this program working:
 
 1) Read through the whole program so you understand the design.
 
-2) Compile and run the program.  It should run three tests, and they should fail.
+2) Compile and run the program. It should run three tests, and they should fail.
 
-3) Fill in the body of reverse_string().  When you get it working, the first test should pass.
+3) Fill in the body of reverse_string(). When you get it working, the first test should pass.
 
-4) Fill in the body of itoc().  When you get it working, the second test should pass.
+4) Fill in the body of itoc(). When you get it working, the second test should pass.
 
-5) Fill in the body of add_digits().  When you get it working, the third test should pass.
+5) Fill in the body of add_digits(). When you get it working, the third test should pass.
 
-6) Uncomment the last test in main.  If your three previous tests pass, this one should, too.
+6) Uncomment the last test in main. If your three previous tests pass, this one should, too.
 
 */
 
@@ -26,20 +25,28 @@ Follow these steps to get this program working:
 #include <assert.h>
 
 /* reverse_string: Returns a new string with the characters reversed.
-
 It is the caller's responsibility to free the result.
-
 s: string
 returns: string
 */
 char *reverse_string(char *s) {
-    //TODO: Fill this in.
-    char sr = [];
+    int len = strlen(s);
+    char* sr = malloc(len*sizeof(char));
+
+    int count = 0;
+    int i;
+    for (i=len-1; i>=0; i--) {
+      sr[count] = s[i];
+      // printf("sr: %s: \n", sr);
+      count++;
+    }
+    sr[count] = '\0';
+    // printf("Original String: %s: \n", s);
+    // printf("Reversed String: %s: \n", sr);
     return sr;
 }
 
 /* ctoi: Converts a character to integer.
-
 c: one of the characters '0' to '9'
 returns: integer 0 to 9
 */
@@ -49,29 +56,42 @@ int ctoi(char c) {
 }
 
 /* itoc: Converts an integer to character.
-
 i: integer 0 to 9
 returns: character '0' to '9'
 */
-char itoc(int i) {
+char itoc (int i) {
     //TODO: Fill this in, with an appropriate assertion.
-    return '0';
+    // char s[1];
+    // char s = malloc(sizeof(char));
+    // s = i + '0';
+    // s = "aaa";
+    // s = sprintf(s,"%d",i); // write formatted data to string
+    // switch(i) {
+    //   case
+    // }
+
+    // printf("Int: %i: \n", i);
+    // printf("Int to string: %c: \n", s);
+    assert(isdigit('0'+i)); // causing segmentation fault?
+
+    return '0'+i;
 }
 
 /* add_digits: Adds two decimal digits, returns the total and carry.
-
 For example, if a='5', b='6', and carry='1', the sum is 12, so
 the output value of total should be '2' and carry should be '1'
-
 a: character '0' to '9'
 b: character '0' to '9'
 c: character '0' to '9'
 total: pointer to char
 carry: pointer to char
-
 */
 void add_digits(char a, char b, char c, char *total, char *carry) {
-    //TODO: Fill this in.
+    int ai = ctoi(a);
+    int bi = ctoi(b);
+    int ci = ctoi(c);
+    int t = ai+bi+ci;
+    printf("Total (int): %i: \n", t);
 }
 
 /* Define a type to represent a BigInt.
@@ -81,9 +101,7 @@ void add_digits(char a, char b, char c, char *total, char *carry) {
 typedef char * BigInt;
 
 /* add_bigint: Adds two BigInts
-
 Stores the result in z.
-
 x: BigInt
 y: BigInt
 carry_in: char
@@ -129,7 +147,6 @@ void add_bigint(BigInt x, BigInt y, char carry_in, BigInt z) {
 }
 
 /* print_bigint: Prints the digits of BigInt in the normal order.
-
 big: BigInt
 */
 void print_bigint(BigInt big) {
@@ -140,9 +157,7 @@ void print_bigint(BigInt big) {
 }
 
 /* make_bigint: Creates and returns a BigInt.
-
 Caller is responsible for freeing.
-
 s: string of digits in the usual order
 returns: BigInt
 */
