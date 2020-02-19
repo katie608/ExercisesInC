ac77b3358b997874818ba3e9857ac8cd21d2aa5b
katie608 <katief650@gmail.com>
2020-02-19 17:02:32 -0500

    Get tee working

--- exercises/ex03/tee.c
+++ exercises/ex03/tee.c
@@ -6,25 +6,33 @@ read from standard input and write to standard output and files
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 
 
-
-int main () {
+int main (int argc, char *argv[]) {
   int opt;
   int a = 0;
   char input[256];
   while((opt = getopt(argc, argv, "a")) != -1)  {
-    if (argv[]) {
-      a = 1;
+    switch(opt) {
+      case 'a':
+        a = 1;
     }
-
-
   }
-
+  argc -= optind;
+  argv += optind;
   while (fgets(input, 256, stdin)) {
-    for(i=1; i<argc; i++) {
-      FILE *out_file = fopen(argv[i], "a");
+    FILE *out_file;
+    int i;
+    for(i=0; i<argc; i++) {
+      if (a) {
+        out_file = fopen(argv[i], "a");
+        a = 1;
+      } else {
+        out_file = fopen(argv[i], "w");
+      }
+
       fputs(input, out_file);
       fclose(out_file);
     }
