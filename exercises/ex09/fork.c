23a15ea01933e68d2b44022bae1cf33e7ca820bb
katie608 <katief650@gmail.com>
2020-04-13 18:36:30 -0400

    Start EX09

--- exercises/ex09/fork.c
+++ exercises/ex09/fork.c
@@ -5,6 +5,14 @@ License: MIT License https://opensource.org/licenses/MIT
 
 */
 
+/*
+1. Check whether the parent and child processes share the same global, heap, and
+stack segments
+global: define an int outside of main. I guess check if it has the same address between children
+heap: dynamic memory. Print address of something that you malloced?
+stack: local variables. Define an int inside a function, check if it has same address between children?
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,14 +26,20 @@ License: MIT License https://opensource.org/licenses/MIT
 // errno is an external global variable that contains
 // error information
 extern int errno;
+// global variable declared in erno.h
+// externs it is declared here but defined somewhere else
 
+//define a global variable to check globals
+int globalVar = 12;
 
 // get_seconds returns the number of seconds since the
 // beginning of the day, with microsecond precision
 double get_seconds() {
-    struct timeval tv[1];
-
-    gettimeofday(tv, NULL);
+  // if you call get seconds, do something, then do something else
+    struct timeval tv[1]; // local variable, array of time value structures
+    // tv needs to be a time value
+    gettimeofday(tv, NULL); //gets "wall clock time"
+    // returns seconds and microseconds as a fraction
     return tv->tv_sec + tv->tv_usec / 1e6;
 }
 
@@ -46,11 +60,18 @@ int main(int argc, char *argv[])
     double start, stop;
     int i, num_children;
 
+    // define a local variable to check stack
+    int localVar = 3;
+
+    // dynamically allocate memory to check heap
+    int *dynamic = malloc(sizeof(int));
+
     // the first command-line argument is the name of the executable.
     // if there is a second, it is the number of children to create.
-    if (argc == 2) {
-        num_children = atoi(argv[1]);
-    } else {
+    if (argc == 2) { // if command line has 2 arguments
+        num_children = atoi(argv[1]); // take 2nd parameter, convert to integer
+        // tells how many children should be created
+    } else { //default is one child
         num_children = 1;
     }
 
@@ -71,7 +92,7 @@ int main(int argc, char *argv[])
         }
 
         /* see if we're the parent or the child */
-        if (pid == 0) {
+        if (pid == 0) { //if 0, that means it is a child
             child_code(i);
             exit(i);
         }
@@ -80,8 +101,9 @@ int main(int argc, char *argv[])
     /* parent continues */
     printf("Hello from the parent.\n");
 
-    for (i=0; i<num_children; i++) {
-        pid = wait(&status);
+    for (i=0; i<num_children; i++) { // run loop once for each child
+        pid = wait(&status); //waits for child process to exit, gets process id
+        // of child process, and status is stored
 
         if (pid == -1) {
             fprintf(stderr, "wait failed: %s\n", strerror(errno));
@@ -91,7 +113,18 @@ int main(int argc, char *argv[])
 
         // check the exit status of the child
         status = WEXITSTATUS(status);
+        // prints process id of child process, value that it returned
         printf("Child %d exited with error code %d.\n", pid, status);
+
+        // check global variables addresses against each other
+        // the addresses of the global variables seem to match
+        printf("Address of global variable: %p\n", &globalVar);
+
+        // check malloced memory addresses
+        printf("Address of dynamic variable: %p\n", &dynamic);
+
+        // check local variable addresses
+        printf("Address of local variable: %p\n", &localVar);
     }
     // compute the elapsed time
     stop = get_seconds();
