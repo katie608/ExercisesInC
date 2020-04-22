aa60e8f6af94acb329e981d8d2cf2aaa473136e6
katie608 <katief650@gmail.com>
2020-04-22 18:07:01 -0400

    Work on echo code

--- exercises/ex10/echo_starter.c
+++ exercises/ex10/echo_starter.c
@@ -17,25 +17,35 @@ void error(char *msg)
     exit(1);
 }
 
-void parent_code(int pipe_to_child[])
+void parent_code(int pipe_to_child[], int pipe_to_parent[])
 {
     // close the read end of pipe to child
     close(pipe_to_child[0]);
 
     // write a string to the child
     char line[] = "I'm proud of you, child.";
-<<<<<<< HEAD
-    ssize_t size = write(pipe_to_child[1], line, strlen(line)+1); // use write system call to send from parent to child
-=======
-    ssize_t size = write(pipe_to_child[1], line, strlen(line)+1);
->>>>>>> 164148ba1b0707c6d7e2dbea6c0f09eeb7d9c862
-    if (size == -1)
+    ssize_t write_size = write(pipe_to_child[1], line, strlen(line)+1); // use write system call to send from parent to child
+    if (write_size == -1)
         error("Parent can't write to child");
-    printf("Parent wrote %ld bytes\n", size);
+    printf("Parent wrote %ld bytes\n", write_size);
+
+
+    // close the write end of pipe to parent
+    close(pipe_to_parent[1]);
+
+    int count = 128;
+    char buffer[count];
+
+    // read from pipe_to_parent
+    ssize_t read_size = read(pipe_to_parent[0], buffer, count); // use read system call
+    if (read_size == -1)
+        error("Parent can't read from child");
+    printf("Parent read %ld bytes.\n", read_size);
+    printf("Parent read: %s\n", buffer);
 }
 
 
-void child_code(int pipe_to_child[])
+void child_code(int pipe_to_child[], int pipe_to_parent[])
 {
     // close the write end of pipe to child
     close(pipe_to_child[1]);
@@ -44,15 +54,25 @@ void child_code(int pipe_to_child[])
     char buffer[count];
 
     // read from pipe_to_child
-<<<<<<< HEAD
-    ssize_t size = read(pipe_to_child[0], buffer, count); // use read system call
-=======
-    ssize_t size = read(pipe_to_child[0], buffer, count);
->>>>>>> 164148ba1b0707c6d7e2dbea6c0f09eeb7d9c862
-    if (size == -1)
+    ssize_t read_size = read(pipe_to_child[0], buffer, count); // use read system call
+    if (read_size == -1)
         error("Child can't read from parent");
-    printf("Child read %ld bytes.\n", size);
+    printf("Child read %ld bytes.\n", read_size);
     printf("Child read: %s\n", buffer);
+
+
+
+    // close the read end of pipe to parent
+    close(pipe_to_parent[0]);
+
+    // write a string to the parent
+    char line[] = "Thanks, parental unit.";
+    ssize_t write_size = write(pipe_to_parent[1], line, strlen(line)+1); // use write system call to send from parent to child
+    if (write_size == -1)
+        error("Child can't write to parent");
+    printf("Child wrote %ld bytes\n", write_size);
+
+
 
     exit(0);
 }
@@ -65,15 +85,20 @@ int main(int argc, char *argv[])
     if (pipe(pipe_to_child) == -1)
         error("Can't create the first pipe");
 
-    /*Fork a child process*/
+    /*Create a second pipe */
+    int pipe_to_parent[2];
+    if (pipe(pipe_to_parent) == -1)
+        error("Can't create the second pipe");
+
+    /*Fork a child process (should stay same I think)*/
     pid_t child_pid = fork();
     if (child_pid == -1)
         error("Can't fork process");
 
     if (child_pid == 0) {
-        child_code(pipe_to_child);
+        child_code(pipe_to_child, pipe_to_parent);
     } else {
-        parent_code(pipe_to_child);
+        parent_code(pipe_to_child, pipe_to_parent);
     }
 
     int status;
